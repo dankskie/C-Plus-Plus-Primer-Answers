d855176a86fe364c7f881790d074f5bb825ee584
dankskie <60952672+dankskie@users.noreply.github.com>
2020-02-11 23:03:19 -0500

    Create 1.1.cpp

---
+++ Chapter 1/1.1.cpp
@@ -0,0 +1,5 @@
+// I recommend using Microsoft Visual Studio if you dont already have an IDE of choice.
+// Start a new project under visual C++ and select Console App to get started.
+// It saves files as .cpp as default.
+// Pressing F5 runs your code.
+// If it's not letting you run, try saving the project first; alternatively go to debug -> start debugging.
