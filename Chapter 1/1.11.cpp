a2edf0c0ede4c6b0500b705444720447304c3a87
dankskie <60952672+dankskie@users.noreply.github.com>
2020-02-11 22:57:07 -0500

    Update 1.11.cpp

--- Chapter 1/1.11.cpp
+++ Chapter 1/1.11.cpp
@@ -6,7 +6,7 @@ int main()
 	std::cout << "Enter two numbers:" << std::endl;
 	int v1 = 0, v2 = 0;
 	std::cin >> v1 >> v2;
-	while (v1 > v2 + 1) {
+	while (v1 > v2 + 1) { //The +1 is so it doesn't print v1 or v2. ex: 2 8 -> 3 4 5 6 7
 		--v1;
 		std::cout << v1 << std::endl;
 	}
