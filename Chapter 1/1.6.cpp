92954e447f9febf14e8bce1b2792d01e8499d4a2
dankskie <60952672+dankskie@users.noreply.github.com>
2020-02-11 22:22:06 -0500

    Create 1.6.cpp

---
+++ Chapter 1/1.6.cpp
@@ -0,0 +1,6 @@
+std::cout << "The sum of " << v1;
+          << " and " << v2;
+          << " is " << v1 + v2 << std::endl;
+       
+//Illegal, the ; at the end of line one ends the statement, making line two a completely standalone statement.
+//getting rid of the ; on lines 1 and 2 would fix it.
