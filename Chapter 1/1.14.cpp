ba3a892f81d46d2d657e1f1c72a630b8466ce565
dankskie <60952672+dankskie@users.noreply.github.com>
2020-02-12 21:23:29 -0500

    Create 1.14.cpp

---
+++ Chapter 1/1.14.cpp
@@ -0,0 +1,2 @@
+//For statements can be denser, intitializing variables and incrementing variables on the same line instead of before and after.
+//A while statement would be more useful when handling multiple variables that you want to declare beforehand.
