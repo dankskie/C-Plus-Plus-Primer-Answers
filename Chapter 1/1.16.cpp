2fbc98e6d16eb66cac0634bd2a16c868103cbc9e
dankskie <60952672+dankskie@users.noreply.github.com>
2020-02-21 13:08:47 -0500

    Update 1.16.cpp

--- Chapter 1/1.16.cpp
+++ Chapter 1/1.16.cpp
@@ -2,16 +2,25 @@
 
 int main()
 {
-	int sum = 0, val = 0, x = 0, y = 0;
+	int sum = 0, val = 0, x = 0, y = 0, z = 0;
 	std::cout << "Please enter numbers to be summed: " << std::endl;
 	while (std::cin >> val) {
-		if (x == 0) { //Prints "The sum of " on the first iteration of the while statement
+		if (x == 0) { //Runs only if it's the first iteration of the while loop
 			std::cout << "The sum of ";
-			++x;
+			++x; //x is used to see if it's the first iteration the of the while loop or not
 		}
 		sum += val;
 		std::cout << val << ", ";
-		y = val; //val doesn't hold after the while, so it's stored in y to be used in line 16
+		y = val; //val isn't stored after the while is done, so it's stored in y instead
+		++z; //z is used to see how many times the while loop has executed
 	}
-	std::cout << "\b" << "\b" << "\b" << "and " << y << " is " << sum << "." << std::endl; //"/b" backspaces cmd
+	if (z > 1) { //If the while loop has executed more than once (user typed more than one numbers)
+		std::cout << "\b" << "\b" << "\b" << "and " << y << " is " << sum << "." << std::endl;
+	}
+	else if (z == 1) { //If the while loop has executed once (user typed one number)
+		std::cout << "\b" << "\b" << " and 0 is 0" << std::endl;
+	}
+	else { //If the while loop has not executed (user typed no integers)
+		std::cout << "The sum of 0 and 0 is 0." << std::endl;
+	}
 }
