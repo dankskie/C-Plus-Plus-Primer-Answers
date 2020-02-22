159cef52fc3eebceaa7212cb6fea768752fde405
dankskie <60952672+dankskie@users.noreply.github.com>
2020-02-22 14:13:06 -0500

    Create 1.17.cpp

---
+++ Chapter 1/1.17.cpp
@@ -0,0 +1,2 @@
+//It counts how many times the integer occurs, tries to read the next cin but since there is none, it stops and returns 0.
+//It initializes cnt, skips the while, then prints line 18/19 and returns 0.
