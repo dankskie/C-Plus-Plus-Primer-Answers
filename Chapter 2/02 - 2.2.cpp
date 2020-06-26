723ae2bdf94366a0e14fd0e5f1bab5567aee4ae0
dankskie <60952672+dankskie@users.noreply.github.com>
2020-06-26 22:00:22 +0000

    Create 02 - 2.2.cpp

---
+++ Chapter 2/02 - 2.2.cpp
@@ -0,0 +1,3 @@
+//I don't even know what a mortgage is.
+//I would assume defaulting to unsigned if the values will never go negative, using double for precision when applicable,
+//and matching types to fit around the expected max and min of the 3 values.
