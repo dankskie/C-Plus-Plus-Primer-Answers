bb7104fc780953f0ff1a220fa0d6fe929a76ba98
dankskie <60952672+dankskie@users.noreply.github.com>
2020-06-26 21:55:43 +0000

    Create 1 - 2.1.cpp

---
+++ Chapter 2/1 - 2.1.cpp
@@ -0,0 +1,3 @@
+//short < int < long < long long
+//unsigned types can't be negative, but can go higher into the positives as a result
+//double is more precise than a float, at a negligibly higher run-time cost
