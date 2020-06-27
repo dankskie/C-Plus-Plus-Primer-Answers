86ddb596f3b42decffcb5bc3060a6bce09bd908d
dankskie <60952672+dankskie@users.noreply.github.com>
2020-06-27 18:20:36 +0000

    Update 05 - 2.5.cpp

--- Chapter 2/05 - 2.5.cpp
+++ Chapter 2/05 - 2.5.cpp
@@ -2,3 +2,19 @@
 //Long single character (extended bit range or some shit)
 //String literal
 //Long string literal (same)
+
+//Int
+//Unsigned int
+//Long
+//Unsigned long
+//Octal
+//Hexadecimal
+
+//Double
+//Float
+//Long
+
+//Int
+//Unsigned int
+//Double
+//Double
