fbede529a7c77229438883e4c0198c5dd49706fa
dankskie <60952672+dankskie@users.noreply.github.com>
2020-06-30 16:59:42 +0000

    Create 09 - 2.9.cpp

---
+++ Chapter 2/09 - 2.9.cpp
@@ -0,0 +1,4 @@
+//Illegal, Can't define a variable in the middle of a command
+//Illegal, curly braces makes it need a narrowing conversion
+//Illegal, would work if you declared wage first
+//Legal, would lose the .14 when it auto changes from double to int though
